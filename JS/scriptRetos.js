c2e9e5df3a6d722e0ba9dfa34c2e5b02e6412321
lautaro <lautaroolguin89@gmail.com>
2022-11-16 15:45:41 -0300

    Creando un if para tex

--- JS/scriptRetos.js
+++ JS/scriptRetos.js
@@ -3,4 +3,7 @@ function creacioncomentarios(tex, tex2){
     element = document.getElementById('submit');
     element = document.setProperty("color", "");
     element = classList.remover('sub2');
+    if(tex.length>=1 && tex2.length>=1){
+        
+    }
 }
