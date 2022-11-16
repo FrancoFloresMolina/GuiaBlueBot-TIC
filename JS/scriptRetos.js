76bdbf32fccad87b275f308b3d3a4ffc2c75eb17
lautaro <lautaroolguin89@gmail.com>
2022-11-16 15:41:22 -0300

    colocando el set para el color de el boton

--- JS/scriptRetos.js
+++ JS/scriptRetos.js
@@ -1,4 +1,5 @@
 /* El commit anterior agrego los archivos*/
 function creacioncomentarios(tex, tex2){
     element = document.getElementById('submit');
+    element = document.setProperty("color", "");
 }
