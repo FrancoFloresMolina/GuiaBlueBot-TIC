9842db0e628b2f2824c305aecd26697b058e1a38
lautaro <lautaroolguin89@gmail.com>
2022-11-16 15:42:58 -0300

    colocando la lista de palabras

--- JS/scriptRetos.js
+++ JS/scriptRetos.js
@@ -2,4 +2,5 @@
 function creacioncomentarios(tex, tex2){
     element = document.getElementById('submit');
     element = document.setProperty("color", "");
+    element = classList.remover('sub2');
 }
