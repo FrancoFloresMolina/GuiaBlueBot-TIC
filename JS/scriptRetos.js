15b0e961f7766fbb3c0f8cffcc68312c80c45a1e
lautaro <lautaroolguin89@gmail.com>
2022-11-17 09:07:34 -0300

    Haciendo cambios

--- JS/scriptRetos.js
+++ JS/scriptRetos.js
@@ -1,13 +1,26 @@
 /* El commit anterior agrego los archivos*/
-function boton(nom) {
-    campo = document.getElementById("texto");
-    h1 = document.createElement("h1");
+function creacioncomentarios(tex, tex2){
+    element = document.getElementById('boton');
+    element = document.setProperty("color", "");
+    element = classList.remover('sub2');
+    if(tex.length>=1 && tex2.length>=1){
 
-    var valor = entrada + " " + "eres bienvenido!!";
-
-    h1.innerHTML = valor;
-    //h1 con el input
-    campo.appendChild(h1);
-    //h1 con el documento (HTML)
-
+        element.classList.remover('sub2');
+        r1= "Hola bienvenido" + tex;
+        document.getElementById("texto").innerHTML=r1;
+        element.classList.add("boton");
+        element.style.setProperty("color", "blue");
+    }else{
+        if(tex.length<1){
+            element.classList.remover("boton");
+            r1= "Porfavor coloque su nombre";
+            document.getElementById("texto").innerHTML=r1;
+            element.classList.add("sub2");
+        }else{
+            element.classList.remover("boton");
+            r1= "Porfavor coloque su apellido";
+            document.getElementById("texto").innerHTML=r1;
+            element.classList.add("sub2");
+        }
+    }
 }
